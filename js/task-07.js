e3cdcd944efe13947b127ccf85397f22d901f812
YuriiHaidai <ura.kiev@gmail.com>
2019-08-07 00:25:48 +0300

    fix task-02,03,05,07 in new rep

---
+++ js/task-07.js
@@ -0,0 +1,17 @@
+let input;
+const numbers = [];
+let total = 0;
+
+do {
+  input = prompt('Введи число');
+  if (input !== null) {
+    numbers.push(Number(input));
+  }
+} while (input !== null);
+
+if (numbers.length !== 0) {
+  for (let number of numbers) {
+    total += number;
+  }
+  console.log(`Общая сумма чисел равна ${total}`);
+}
